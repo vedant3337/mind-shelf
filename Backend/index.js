b8463bf7646821a51843c1b096e03b48ff4cfba0
Vedant Patel <vedant3337>
2025-06-28 12:15:46 +0530

    version 2.0

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-
 import bookRoute from "./route/book.route.js";
 import userRoute from "./route/user.route.js";
 
